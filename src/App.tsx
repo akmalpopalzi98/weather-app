f3ad1b8afa69c060693f8e3bfcc82ae74c06c264
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-24 20:54:54 +0000

    chore: adding new deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Box, Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material";
 import SearchBar from "./components/SearchBar";
 import { SearchBarProvider } from "./context/SearchBarContext";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import "./app.css";
 
 const theme = createTheme({
   typography: {
@@ -17,13 +19,12 @@ function App() {
           sx={{
             background: "linear-gradient(brown,blue)",
             textAlign: "center",
-            width: "700px",
             margin: "0 auto",
             height: "900px",
           }}
         >
           <Typography sx={{ fontWeight: 700, fontSize: "50px" }}>
-            Weather in
+            Weather <WbSunnyIcon fontSize="large" sx={{ color: "yellow" }} />
           </Typography>
           <SearchBar />
         </Box>
