df97392e60971f22ef2eeff5cd4d927a83326c7f
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-26 00:49:56 +0000

    chore: updated structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Box, Typography } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material";
-import SearchBar from "./components/SearchBar";
 import { SearchBarProvider } from "./context/SearchBarContext";
-import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import "./app.css";
+import MainPage from "./pages/MainPage";
 
 const theme = createTheme({
   typography: {
@@ -15,19 +13,7 @@ function App() {
   return (
     <SearchBarProvider>
       <ThemeProvider theme={theme}>
-        <Box
-          sx={{
-            background: "linear-gradient(brown,blue)",
-            textAlign: "center",
-            margin: "0 auto",
-            height: "900px",
-          }}
-        >
-          <Typography sx={{ fontWeight: 700, fontSize: "50px" }}>
-            Weather <WbSunnyIcon fontSize="large" sx={{ color: "yellow" }} />
-          </Typography>
-          <SearchBar />
-        </Box>
+        <MainPage />
       </ThemeProvider>
     </SearchBarProvider>
   );
