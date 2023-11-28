9cc5a75a844ac251dc2993b907f2869e0f16d382
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-28 22:27:40 +0000

    chore: adding weather page

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -1,5 +1,23 @@
+import { Box } from "@mui/material";
+
 const WeatherPage = () => {
-  return "weather page";
+  const location = navigator.geolocation;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        height: "70%",
+        marginTop: "50px",
+      }}
+    >
+      <Box
+        sx={{ width: "90%", backgroundColor: "rgba(150, 150, 150, 0.7)" }}
+      ></Box>
+    </Box>
+  );
 };
 
 export default WeatherPage;
