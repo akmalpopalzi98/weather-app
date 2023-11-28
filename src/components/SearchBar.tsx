7b86cb35cb95c6730b1d1edd2f5d24ddc8727667
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-28 21:50:25 +0000

    chore: adding event listeners

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,13 +1,20 @@
 import { Box } from "@mui/material";
-import { ChangeEvent, useContext } from "react";
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
 import { SearchBarContext } from "../context/SearchBarContext";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
   const { setSearchTerm } = useContext(SearchBarContext);
+  const [value, setValue] = useState("");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setValue("");
   };
 
   return (
@@ -17,20 +24,33 @@ const SearchBar = () => {
         justifyContent: "center",
         marginTop: "10px",
       }}
+      component={"form"}
+      onSubmit={handleSubmit}
     >
+      <i
+        style={{
+          backgroundColor: "white",
+          width: "50px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          cursor: "pointer",
+        }}
+      >
+        <SearchIcon />
+      </i>
       <input
         style={{
           width: "40%",
           height: "35px",
           border: "none",
           fontSize: "15px",
+          textAlign: "center",
         }}
         placeholder="Search"
         onChange={handleChange}
+        value={value}
       />
-      <button style={{ width: "40px" }}>
-        <SearchIcon />
-      </button>
     </Box>
   );
 };
