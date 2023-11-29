d68ef053554db88da9ead8c5f5bba6cbc4da24be
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-29 15:45:14 +0000

    chore: updating useEffect hook

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,21 +2,26 @@ import { Box } from "@mui/material";
 import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
 import { SearchBarContext } from "../context/SearchBarContext";
 import SearchIcon from "@mui/icons-material/Search";
-import { get_data } from "../API";
+import { get_current_data } from "../API";
 
 const SearchBar = () => {
-  const { searchTerm, setSearchTerm, setWeatherData } =
+  const { searchTerm, setSearchTerm, setCurrentWeather } =
     useContext(SearchBarContext);
   const [value, setValue] = useState("");
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await get_data(searchTerm);
-  //     setWeatherData(response.data);
-  //     console.log(response.data);
-  //   };
-  //   fetchData();
-  // }, []);
+  const fetchCurrentData = async (city: string) => {
+    try {
+      const response = await get_current_data(city);
+      setCurrentWeather(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentData(searchTerm);
+  }, []);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -25,8 +30,8 @@ const SearchBar = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(searchTerm);
     setValue("");
+    fetchCurrentData(searchTerm);
   };
 
   return (
