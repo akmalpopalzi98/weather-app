ae5496a841420dfc0634fb7d632704316f4ea47e
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-11-29 15:44:57 +0000

    chore: adding new API

--- src/API.tsx
+++ src/API.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const get_data = async (city: string) => {
+const get_current_data = async (city: string) => {
   const request = await axios.get(
     "https://api.openweathermap.org/data/2.5/weather",
     {
@@ -14,4 +14,18 @@ const get_data = async (city: string) => {
   return request;
 };
 
-export { get_data };
+const get_forecast_data = async (city: string) => {
+  const request = await axios.get(
+    "https://api.openweathermap.org/data/2.5/forecast",
+    {
+      params: {
+        q: city || "london",
+        appid: "39beb0058dea7387317a41b76719c76e",
+      },
+    }
+  );
+
+  return request;
+};
+
+export { get_current_data, get_forecast_data };
