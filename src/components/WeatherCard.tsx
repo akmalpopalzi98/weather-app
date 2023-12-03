941b391714a2bd182074952583f9dcc94920fb00
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-12-03 00:22:33 +0000

    chore: refactored code

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,48 @@
+import { Box, Typography } from "@mui/material";
+import CurrentWeatherType from "../types";
+
+const units = {
+  CELSIUS: "°C",
+};
+
+const WeatherCard = ({
+  weatherData,
+  currentTemp,
+  weatherDesc,
+}: {
+  weatherData: CurrentWeatherType;
+  currentTemp: number;
+  weatherDesc: string;
+}) => {
+  const formattedCurrentDate = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <Box
+      sx={{
+        backgroundColor: "rgba(160, 64, 0, 0.8)",
+        height: "100%",
+        width: "30%",
+        display: "flex",
+        flexDirection: "column", // Display items vertically
+        justifyContent: "space-evenly", // Space items evenly vertically
+        alignItems: "center", // Center items horizontally
+      }}
+    >
+      <Typography sx={{}}>{weatherData.name}</Typography>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+      />
+      <Typography sx={{ fontSize: "60px" }}>
+        {currentTemp + units.CELSIUS}
+      </Typography>
+      <Typography>{weatherDesc}</Typography>
+      <Typography>Today - {formattedCurrentDate}</Typography>
+    </Box>
+  );
+};
+
+export default WeatherCard;
