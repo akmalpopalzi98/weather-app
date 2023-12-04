530bea33dac251a268494fccdd3adafad6a9433b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-12-04 15:47:45 +0000

    chore: updated file

--- src/components/MiniWeatherCard.tsx
+++ src/components/MiniWeatherCard.tsx
@@ -1,7 +1,14 @@
 import { Box } from "@mui/material";
+import { MiniCardType } from "../types";
 
-const MiniWeatherCard = () => {
-  return <Box>MiniCard</Box>;
+const MiniWeatherCard = ({ data }: { data: MiniCardType }) => {
+  return (
+    <Box sx={{ width: "80%", height: "25%", backgroundColor: "green" }}>
+      <img
+        src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+      />
+    </Box>
+  );
 };
 
 export default MiniWeatherCard;
