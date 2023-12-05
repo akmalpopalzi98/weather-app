5b565c4901d53dc73cb10c557a93bfc20a68170a
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2023-12-05 14:08:42 +0000

    feat: added geolocation api to get client location

--- src/API.tsx
+++ src/API.tsx
@@ -1,33 +1,61 @@
 import axios from "axios";
 
-const get_current_data = async (city: string) => {
-  const request = await axios.get(
-    "https://api.openweathermap.org/data/2.5/weather",
-    {
-      params: {
-        q: city || "london",
-        appid: "39beb0058dea7387317a41b76719c76e",
-        units: "metric",
-      },
-    }
-  );
-
-  return request;
+const get_current_data = async (city: string, lat?: number, lon?: number) => {
+  if (!lat && !lon) {
+    const request = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          q: city || "London",
+          appid: "39beb0058dea7387317a41b76719c76e",
+          units: "metric",
+        },
+      }
+    );
+    return request;
+  } else {
+    const request = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          appid: "39beb0058dea7387317a41b76719c76e",
+          units: "metric",
+          lat: lat,
+          lon: lon,
+        },
+      }
+    );
+    return request;
+  }
 };
 
-const get_forecast_data = async (city: string) => {
-  const request = await axios.get(
-    "https://api.openweathermap.org/data/2.5/forecast",
-    {
-      params: {
-        q: city || "london",
-        appid: "39beb0058dea7387317a41b76719c76e",
-        units: "metric",
-      },
-    }
-  );
-
-  return request;
+const get_forecast_data = async (city: string, lat?: number, lon?: number) => {
+  if (!lat && !lon) {
+    const request = await axios.get(
+      "https://api.openweathermap.org/data/2.5/forecast",
+      {
+        params: {
+          q: city || "London",
+          appid: "39beb0058dea7387317a41b76719c76e",
+          units: "metric",
+        },
+      }
+    );
+    return request;
+  } else {
+    const request = await axios.get(
+      "https://api.openweathermap.org/data/2.5/forecast",
+      {
+        params: {
+          appid: "39beb0058dea7387317a41b76719c76e",
+          units: "metric",
+          lat: lat,
+          lon: lon,
+        },
+      }
+    );
+    return request;
+  }
 };
 
 export { get_current_data, get_forecast_data };
